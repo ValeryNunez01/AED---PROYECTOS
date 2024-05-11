5e50b9f7a2eeec1c239351d91a55829ba2eaf30d
ValeryNunez01 <vnunezr1900@alumno.ipn.mx>
2024-05-10 18:39:45 -0600

    CAMBIOS EN sEArchivo

--- sEArchivo.cpp
+++ sEArchivo.cpp
@@ -5,6 +5,8 @@
 #include <string>
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
+#include <cstring>
 
 #define NOMBRE_ARCHIVO "conocimiento.csv"
 #define MAX 50
@@ -53,15 +55,15 @@ int main(int argc, char** argv) {
 
 struct nodo{
 	char sentencia[MAX];
-	int *a = NULL;	
+	nodo *a = NULL;	
 };
 
 using namespace std;
-typedef nodo * apu_nodo;
+//typedef nodo * apu_nodo;
 
 int main(int argc, char** argv) {
 	
-	apu_nodo auxiliar = NULL;
+	nodo *auxiliar2 = NULL, *actual = NULL;
 	
 	ifstream file;
 	file.open("conocimiento.csv");
@@ -94,20 +96,64 @@ int main(int argc, char** argv) {
 	//CREACION Y LLENADO DE ARBOL
 	cont = 0;
 	int i = 0;
-	int tam = 0;
-	int ant = 0;
-	while(i < S){
-		auxiliar = new(nodo);
-		auxiliar->a = (int*)malloc(tam*sizeof(int));
-		//while(idPadres[cont] == ant){
-		//	tam++;
-		//}
+	int tam;
+	int ant = 1;
+	int pos = 1;
+	int posLen = 1;
+ 	
+	
+	nodo *auxiliar = new nodo();
+	
+	char chars[ss[0].length() + 1];
+    strcpy(chars, ss[0].c_str());
+	strcpy(auxiliar->sentencia, chars);
+	
+	nodo *raiz = new nodo();
+	
+	raiz = auxiliar;
+	
+	while(i < 2){
+		
+		tam = 0;
+		
+		while(idPadres[pos] == ant){
+			tam++;
+			pos++;
+		}
+		ant = idPadres[pos];
+		//cout<<"idPadres:"<<idPadres[pos]<<endl;
+		//cout<<"tam: "<<tam<<endl;
+		auxiliar->a = (nodo*)malloc(tam*sizeof(nodo));
+		
+		for(cont=0 ; cont<tam ; cont++){
+			nodo *auxiliar2 = new nodo();
+			
+			char chars[ss[posLen].length() + 1];
+    		strcpy(chars, ss[posLen].c_str());
+			strcpy(auxiliar2->sentencia, chars);
+			
+			cout<<"SF "<<idPadres[pos]<<" :"<<auxiliar2->sentencia<<endl;
+			auxiliar->a[cont] = *auxiliar2;
+			posLen++;
+		}
+		
+		auxiliar = &auxiliar->a[0];
+		//auxiliar = auxiliar 
+
 		i++;
 	}
+	cout<<"hola"<<endl;
 	
-	for(int i=0 ; i<5; i++){
-		cout<<ss[i]<<" "<<endl;
-	}
+	cout<<raiz->sentencia<<endl;
+	auxiliar = &raiz->a[0];
+	cout<<"nivel 1: "<<auxiliar->sentencia<<endl;
+	auxiliar = &raiz->a[1];
+	cout<<"nivel 1: "<<auxiliar->sentencia<<endl;
+	
+	auxiliar = &auxiliar->a[0];
+	cout<<"S: "<<auxiliar->sentencia<<endl;
+
+
 	
 	return 0;
 }
